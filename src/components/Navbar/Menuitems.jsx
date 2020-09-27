0faaf26c84f9af6c779a2f14b0014d51330fa492
AlekseyOgorodnikov <ogorodnikovalexey@gmail.com>
2020-09-28 02:00:02 +0400

    complit

---
+++ src/components/Navbar/Menuitems.jsx
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: "Главная",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Услуги",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Продукты",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Контакты",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Регистрация",
+        url: "#",
+        cName: "nav-links-mobile"
+    }
+]
